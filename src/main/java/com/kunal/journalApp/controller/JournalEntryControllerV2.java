dfab012ad3def0c1e5d9b3a3b87113119af520e1
Kunal Sachdev <kunalsachdev456@gmail.com>
2025-11-05 19:22:43 +0530

    Use ResponseEntity and send appropraite response with httpstatus codes

--- src/main/java/com/kunal/journalApp/controller/JournalEntryControllerV2.java
+++ src/main/java/com/kunal/journalApp/controller/JournalEntryControllerV2.java
@@ -4,13 +4,12 @@ import com.kunal.journalApp.entity.JournalEntry;
 import com.kunal.journalApp.service.JournalEntryService;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
@@ -20,37 +19,52 @@ public class JournalEntryControllerV2 {
     private JournalEntryService journalEntryService;
 
     @GetMapping
-    public List<JournalEntry> getAll(){
-        return journalEntryService.getAll();
+    public ResponseEntity<?> getAll(){
+        List<JournalEntry> all= journalEntryService.getAll();
+        if (all != null && !all.isEmpty()){
+            return new ResponseEntity<>(journalEntryService.getAll(), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
     }
 
     @PostMapping
-    public boolean createEntry(@RequestBody JournalEntry newEntry) {
-        newEntry.setDate(LocalDateTime.now());
-        journalEntryService.saveEntry(newEntry);
-        return true;
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry newEntry) {
+        try {
+            newEntry.setDate(LocalDateTime.now());
+            journalEntryService.saveEntry(newEntry);
+            return new ResponseEntity<>(newEntry, HttpStatus.CREATED);
+        }
+        catch(Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
     @GetMapping("id/{journalEntryId}")
-    public JournalEntry getJournalEntryById(@PathVariable ObjectId journalEntryId){
-        return journalEntryService.findById(journalEntryId).orElse(null);
+    public ResponseEntity<JournalEntry> getJournalEntryById(@PathVariable ObjectId journalEntryId){
+        Optional<JournalEntry> journalEntry = journalEntryService.findById(journalEntryId);
+        if (journalEntry.isPresent()) {
+            return new ResponseEntity<>(journalEntry.get(), HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @DeleteMapping("id/{journalEntryId}")
-    public boolean deleteJournalEntryById(@PathVariable ObjectId journalEntryId){
+    public ResponseEntity<?> deleteJournalEntryById(@PathVariable ObjectId journalEntryId){
         journalEntryService.deleteById(journalEntryId);
-        return true;
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PutMapping("id/{journalEntryId}")
-    public JournalEntry updateJournalEntryById(@PathVariable ObjectId journalEntryId, @RequestBody JournalEntry updatedEntry){
+    public ResponseEntity<?> updateJournalEntryById(@PathVariable ObjectId journalEntryId, @RequestBody JournalEntry updatedEntry){
         JournalEntry oldJournalEntry = journalEntryService.findById(journalEntryId).orElse(null);
         if(oldJournalEntry != null){
             oldJournalEntry.setTitle(updatedEntry.getTitle() != null && !updatedEntry.getTitle().equals("") ? updatedEntry.getTitle() : oldJournalEntry.getTitle());
             oldJournalEntry.setContent(updatedEntry.getContent() != null && !updatedEntry.getContent().equals("") ? updatedEntry.getContent() : oldJournalEntry.getContent());
+            journalEntryService.saveEntry(oldJournalEntry);
+            return new ResponseEntity<>(oldJournalEntry, HttpStatus.OK);
         }
-        journalEntryService.saveEntry(oldJournalEntry);
-        return oldJournalEntry;
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
